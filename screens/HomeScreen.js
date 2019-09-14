9fdcaa940ba08bae90794c18b956daf787ef4afb
ColeWalker <colegrwalker@gmail.com>
2019-09-14 08:56:12 -0400

    updated settings button styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -23,40 +23,49 @@ import { Ionicons } from '@expo/vector-icons'
 
 
 const ConditionalWrap = ({condition, wrap, children}) => condition ? wrap(children) : <View style={[spacing.mainContainer, spacing.column, spacing.center, spacing.mainTopPad]}>{children}</View>;
+
 const styles= StyleSheet.create({
   settingsButton:{
-    position: "absolute",
-    top: 30,
-    right: 20,
+    position:"absolute",
+    right:0,
+  },
+  headingRow:{
+    display:"flex",
+    width:"100%",
+    flexDirection:"row",
+    justifyContent:"center",
+    alignItems:"center",
   }
+  
 });
 class HomeScreen extends React.Component {
   render(){
     return (
-
-    
-        <LinearGradient 
+      <LinearGradient 
         colors={this.props.background.length > 1 ? this.props.background : [this.props.background[0], this.props.background[0]]} 
-        style={[spacing.mainContainer, spacing.column, spacing.center, spacing.mainTopPad]} >
-       
-  
+        style={[spacing.mainContainer, spacing.column, spacing.center, spacing.mainTopPad]} 
+      >  
+        {this.props.countdownName && 
+          ( <View 
+              style={styles.headingRow}
+            >
+              <Text style={[typography.heading, typography.center, {color: this.props.textColor, flex:1, alignSelf:"center"}]}>{this.props.countdownName}</Text>
+              <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('Settings')}
+                style={styles.settingsButton}
+              >
+                <Ionicons name="md-settings" size={50} color={this.props.textColor}/>
+              </TouchableOpacity>
 
-      
-      {!this.props.countDownName&& ( <Text style={[typography.heading, typography.center, {color: this.props.textColor}]}>{this.props.countdownName}</Text>)}
-      <ScrollView
+            </View>)
+        }
+        <ScrollView
+          contentContainerStyle={{alignItems:'center', flex: 1}}
+          >
+          <CountDown /> 
+          
+        </ScrollView>
         
-        contentContainerStyle={{alignItems:'center', flex: 1}}
-        >
-        <CountDown /> 
-        
-      </ScrollView>
-        
-      <TouchableOpacity
-         onPress={() => this.props.navigation.navigate('Settings')}
-         style={styles.settingsButton}
-      >
-        <Ionicons name="md-settings" size={50} color={this.props.textColor}/>
-      </TouchableOpacity>
     </LinearGradient>
   );}
 
