e4cb1146d9651680c8ae2a47ab478443193c2b95
ColeWalker <colegrwalker@gmail.com>
2019-09-14 20:43:13 -0400

    added image picker updated styling

--- screens/ColorPickerScreen.js
+++ screens/ColorPickerScreen.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import { Text, TextInput, View, Button, KeyboardAvoidingView, StyleSheet, TouchableOpacity} from 'react-native'
+import { Alert, Image, ScrollView, Text, TextInput, View, Button, KeyboardAvoidingView, StyleSheet, TouchableOpacity} from 'react-native'
 import { ColorPicker, fromHsv } from 'react-native-color-picker'
-import { changeBackgroundRingColor, changeProgressColor, changeTextColor, changeBackground } from '../actions/index.js'
+import { changeBackgroundRingColor, changeProgressColor, changeTextColor, changeBackground, changeBackgroundImageURI } from '../actions/index.js'
 import { connect} from 'react-redux'
 import { Ionicons } from '@expo/vector-icons';
 import { appStyle } from '../styles/index'
+import * as ImagePicker from "expo-image-picker"
+import * as Permissions from "expo-permissions"
+import Constants from "expo-constants"
+import {MaterialIcons}from "@expo/vector-icons"
 
 const styles= StyleSheet.create({
     colorArea:{
@@ -32,6 +36,36 @@ class ColorPickerScreen extends Component {
         }
     };
 
+    saveHandler(){
+        Alert.alert(
+            "Confirm Changes",
+            "Do you want to save the changes you have made?",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () =>{
+
+                    }
+                },
+                {
+                    text: "Confirm",
+                    onPress: () => {
+                        if(this.state.imageSelected){
+                            this.props.changeBackgroundImageURI(this.state.imageURI);
+                        }
+                        else{
+                            this.props.navigation.getParam('type', 'background')=="background" && this.props.changeBackgroundImageURI(null);
+                            this.changeColor(this.state.colors);
+                        }
+                        
+                        this.props.navigation.navigate('Settings');
+                    }
+                },
+                
+            ]
+        )
+    }
+
     static defaultProps = {
         type: "background",
     }
@@ -40,16 +74,24 @@ class ColorPickerScreen extends Component {
         colors: ['#ffffff'],
         index: 0,
         maxIndex: 1,
+        isBackground: false,
+        imageURI: "",
+        imageSelected: false,
     }
     
 
     componentDidMount(){
+        this.getPermissionAsync();
         if(this.props.navigation.getParam('type', 'background')=="background"){
-            this.setState({colors: this.props.background});
+            this.setState({
+                colors: this.props.background,
+                isBackground: true,
+            });
         }
     }
     
     colorChangeHandler(color, index = 0){
+            this.setState({imageSelected:false, imageURI: null});
             let newColors = [...this.state.colors];
             newColors.splice(index, 1, color);
             this.setState({colors: newColors});
@@ -61,6 +103,7 @@ class ColorPickerScreen extends Component {
     //type: "background","backgroundring","progresscolor","textcolor"
     changeColor(colors){
         const type = this.props.navigation.getParam('type', 'background');
+        
         switch(type){
             case "background":
                 this.props.changeBackground(colors);
@@ -102,26 +145,85 @@ class ColorPickerScreen extends Component {
         });
     }
 
+
+
+    getPermissionAsync = async () => {
+        if (Constants.platform.ios) {
+          const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+          if (status !== 'granted') {
+            alert('Sorry, we need camera roll permissions to make this work!');
+          }
+        }
+      }
+
+
+    changeImage= async ()=>{
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+
+        });
+        if(!result.cancelled){
+            this.setState({
+                imageSelected: true,
+                imageURI: result.uri
+            });
+        }
+        
+    }
+
     
 
     changeIndex(index){
         (index <= this.state.colors.length) && this.setState({index:index});
     }
 
+    
+
     render() {
         return (
             <View style={appStyle.mainContainer}>
-                <Text>Tap the center circle to select your color.</Text>
-                <ColorPicker
-                    onColorChange={newColor=>{
-                             this.colorChangeHandler(fromHsv(newColor), this.state.index);
-                    }}
-                    onColorSelected={newColor=>this.colorChangeHandler(newColor, this.state.index)}
-                    style={{flex:3,
-                        paddingBottom: 20,
-                    }}
-                />
-                <View style={[{flex:1}, appStyle.row]}>
+
+                <View style={{flex:1}}>
+                    <View style={[appStyle.row, {flexDirection:"row-reverse",justifyContent:"space-between"}]}>
+                       
+                        <MaterialIcons.Button
+                            name="save"
+                            backgroundColor="#2b2b2b"
+                            style={{color: "#ffffff", alignSelf:"flex-end"}}
+                            onPress={()=>{
+                                this.saveHandler();
+                            }} 
+                        >
+                            
+                            Save Changes
+                        </MaterialIcons.Button>
+                        {this.state.isBackground &&
+                        <MaterialIcons.Button
+                            name="image"
+                            backgroundColor="#2b2b2b"
+                            style={{color: "#ffffff",}}
+                            onPress={()=>{
+                                this.changeImage();
+                            }} 
+                        >
+                            Select Background Image
+                        </MaterialIcons.Button>}
+                        
+                    </View>
+                    <Text>Tap the center circle to select your color.</Text>
+                    
+                    <ColorPicker
+                        onColorChange={newColor=>{
+                                 this.colorChangeHandler(fromHsv(newColor), this.state.index);
+                        }}
+                        onColorSelected={newColor=>this.colorChangeHandler(newColor, this.state.index)}
+                        style={{flex:1,
+                            paddingBottom: 20,
+                        }}
+                    />
+                </View>
+                <ScrollView style={{flex:6, paddingHorizontal:20}}>
+                <View style={[{flex:2, flexWrap:"wrap", width: "100%"}, appStyle.row]}>
                     {this.state.colors.map((currentValue, index)=>{
                             return (
                                 <TouchableOpacity onPress={()=>{this.changeIndex(index)}} key={index}>
@@ -133,7 +235,7 @@ class ColorPickerScreen extends Component {
                     } 
                     {this.props.navigation.getParam('type', 'background')==="background" 
                         && (
-                        <View style={[appStyle.row, {flex:1}]}>
+                        <View style={[appStyle.row]}>
                             <TouchableOpacity onPress={()=>{
                                 this.addBackgroundColor();
                             }}>
@@ -165,13 +267,16 @@ class ColorPickerScreen extends Component {
                         keyboardType={"default"}
                     />
                 </KeyboardAvoidingView>
-                <Button
-                    title="Save Color"
+                {this.state.isBackground && (<Button
+                    title="Select Background Image"
                     onPress={()=>{
-                        this.changeColor(this.state.colors);
-                        this.props.navigation.navigate('Settings');
-                    }} 
-                />
+                        this.changeImage();
+                    }}
+                />)}
+                {this.state.imageSelected &&
+                (<Image source={{uri: this.state.imageURI}} style={{width:100, height:100}}/>)}
+               
+                </ScrollView>
             </View>
         )
     }
@@ -183,4 +288,4 @@ const mapStateToProps = state => ({
     textColor: state.textColor
 });
 
-export default connect(mapStateToProps, { changeBackground, changeBackgroundRingColor, changeProgressColor, changeTextColor })(ColorPickerScreen);
+export default connect(mapStateToProps, { changeBackground, changeBackgroundRingColor, changeProgressColor, changeTextColor, changeBackgroundImageURI })(ColorPickerScreen);
